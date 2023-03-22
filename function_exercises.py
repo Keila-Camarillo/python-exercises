ebbc3e466b323567a2d511ddf127de19e61bb4ef
Keila-Camarillo <keila.a.camarillo@gmail.com>
2023-03-22 10:35:33 -0500

    add import exercise

--- function_exercises.py
+++ function_exercises.py
@@ -5,7 +5,6 @@ def is_two(str_input):
         return True
     return False
 
-print(is_two(2.0))
 
 
 
@@ -19,7 +18,7 @@ def is_vowel(str_vowel):
     return False
             
     
-print(is_vowel('mabd'))
+
 
 
 '''3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.'''
@@ -31,7 +30,7 @@ def is_consonant(str_consonant):
                 return True
         return False
     
-print(is_consonant('cambodia'))
+
 
 
 '''4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.'''
@@ -42,10 +41,10 @@ def cap_consonant(word):
             return word.capitalize()
         return 'First letter is not a consonant'
         
-print(cap_consonant("keila"))
+
 
 '''5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.'''
-def calculate_tip(tip_per bill_total):
+def calculate_tip(tip_per, bill_total):
     if 0 < tip_per < 1:
         return tip_per * bill_total
     elif tip_per > 1:
@@ -75,7 +74,7 @@ def handle_commas(str_num):
                 return int(x)
     return "Not a valid entry"
 
-handle_commas('1,000')
+
     
     
 '''8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).'''
@@ -92,7 +91,7 @@ def get_letter_grade(grade):
         else:
             return 'F'
     return 'Invalid entry'
-print(get_letter_grade(88))
+
 
 '''9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.'''
 vowels = 'aeiouAEIOU'
@@ -102,7 +101,19 @@ def remove_vowels(word):
             strip_word = re.sub(r"[aeiouAEIOU]", "", word)
             return strip_word
     return 'No vowels present'
-print(remove_vowels('jacksOn'))
+
+
+
+alpha_and_special = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.!/?'
+def remove_alpha(word):
+    for char in word:
+        if char in alpha_and_special:
+            strip_word = re.sub(r"[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,.!/?]", "", word)
+            x = strip_word.strip()
+            return x
+    return 'No vowels present'
+
+
 
 '''10. Define a function named normalize_name. It should accept a str+ing and return a valid python identifier, that is:
 anything that is not a valid python identifier should be removed
@@ -115,11 +126,11 @@ First Name will become first_name
 % Completed will become completed'''
 def normalize_name(name):
     strip_name = name.strip().lower()
-    remove_char = re.sub(r"[!@#$%^&*)(><,./?}{\|+=~`]", "", strip_name)
+    remove_char = re.sub(r"[!@#$%^&*)(><,/?}{\|+=~`]", "", strip_name)
     normal_name = re.sub(r" ", "_", remove_char)
     normal_name = normal_name.strip('_')
     return normal_name
-print(normalize_name('% Completed'))
+
 
 '''11. Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 cumulative_sum([1, 1, 1]) returns [1, 2, 3]
@@ -134,5 +145,3 @@ def cumulative_sum(ls):
         some_sums.append(total)
     return some_sums
 
-cumulative_sum(my_list)
-    
